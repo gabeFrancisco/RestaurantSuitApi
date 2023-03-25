9b5dc705b955732c56f2035fce9e061486a5d78b
gabeFrancisco <gabegit2@gmail.com>
2023-03-25 14:42:57 -0200

    Created Product DTO and service

---
+++ src/RecantosSystem.Api/DTOs/ProductDTO.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecantosSystem.Api.DTOs
+{
+    public class ProductDTO
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
